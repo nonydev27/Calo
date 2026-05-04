87ab40eb40d41a04a502a367dd3bf555faa510f7
nonydev <karldjansi123@gmail.com>
2026-05-04 23:34:13 +0000

    classified

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,9 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerTitle: "Calo" }} />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(tabs)" />
+    </Stack>
+  );
 }
